53322b3d7542ebbe3b7335ab788e43c2cb40fa20
Miro Macko <miro.macko@nozdormu.net>
2023-03-14 21:29:37 +0000

    playing

--- scheduled.py
+++ scheduled.py
@@ -7,18 +7,20 @@ from slack_sdk.errors import SlackApiError
 
 logging.basicConfig(level=logging.DEBUG)
 
+# https://www.freecodecamp.org/news/how-to-build-a-basic-slackbot-a-beginners-guide-6b40507db5c5/
 def sendMessage(slack_client, msg):
   # make the POST request through the python slack client
   
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
-      channel='#test',
+      channel='#test-jedu-bomby',
       text=msg
     )#.get()
   except SlackApiError as e:
     logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
     logging.error(e.response)
+
 
 if __name__ == "__main__":
   SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
@@ -27,7 +29,7 @@ if __name__ == "__main__":
 
   # # For testing
   msg = "Good Morning!"
-  schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
+  schedule.every(11).seconds.do(lambda: sendMessage(slack_client, msg))
 
   # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
   logging.info("entering loop")
